a376b3a198b3ec35acb12547b911babb202c5b76
Darkspice <darkspice@yandex.ru>
2018-07-18 19:23:04 +0400

    Строки таблицы
    Объединение таблицы по шести строкам всесто семи

--- frac_info.php
+++ frac_info.php
@@ -139,7 +139,7 @@
 		usort($faction_stats, 'myCmp');
 
 
-		$content = '<td class="th" colspan="7"><div class="title">Убийства</div></td>'; 
+		$content = '<td class="th" colspan="6"><div class="title">Убийства</div></td>'; 
 		if(isset($list_of_faction_kills[$frac_id]))
 		{
 			krsort($list_of_faction_kills[$frac_id]);
@@ -157,7 +157,7 @@
 				</tr>";
 			}
 		}
-		$content .= '<td class="th" colspan="7"><div class="title">Смерти</div><a name="deaths"></a></td>';
+		$content .= '<td class="th" colspan="6"><div class="title">Смерти</div><a name="deaths"></a></td>';
 		if(isset($list_of_faction_deaths[$frac_id]))
 		{
 			krsort($list_of_faction_deaths[$frac_id]);
